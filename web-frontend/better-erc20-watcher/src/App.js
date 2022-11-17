087bb924a12f56820100dfdb8af62d83dfbd37fa
bradj00 <brjohnson00@gmail.com>
2022-11-17 10:40:46 -0600

    brought in material-ui dashboard scaffolding

--- web-frontend/better-erc20-watcher/src/App.js
+++ web-frontend/better-erc20-watcher/src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  style={{backgroundColor:'#333',position:'absolute', width:'100vw', height:'100vh', display:'flex', justifyContent:'center', alignItems:'center', fontSize:'1vw', color:'#fff'}}>
+      <Dashboard />
     </div>
   );
 }
