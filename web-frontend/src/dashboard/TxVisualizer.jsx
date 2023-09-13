cf2f3928811c8f61f7cbfc39a802436333d27230
bradj00 <brjohnson00@gmail.com>
2023-09-13 16:23:38 -0500

    begin upgrade to Tx Visualizer

--- web-frontend/src/dashboard/TxVisualizer.jsx
+++ web-frontend/src/dashboard/TxVisualizer.jsx
@@ -1,3 +1,20 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import FastRewindIcon from '@mui/icons-material/FastRewind';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import FastForwardIcon from '@mui/icons-material/FastForward';
+import ClearIcon from '@mui/icons-material/Clear';
+import Button from '@mui/material/Button';
+import ReactSlider from 'react-slider';
+
+
+import Datetime from 'react-datetime';
+import "react-datetime/css/react-datetime.css";
+
+import "./styles.css";
 import React, { useContext, useState, useEffect, useRef, useCallback } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -10,6 +27,9 @@ import { ForceGraph3D } from "react-force-graph";
 import "../App.css";
 import SettingsOverscanIcon from '@mui/icons-material/SettingsOverscan';
 import * as THREE from 'three';
+// import * as d3 from 'd3';
+import Web3 from 'web3';
+
 
 TimeAgo.addDefaultLocale(en);
 
@@ -29,11 +49,11 @@ function determineGroup(address, TxSummaryData) {
 
     const ratio = sent / received;
 
-    if (ratio > 1.2) return { group: 1, color: 'red' }; // send is "much greater" than receive
-    if (ratio > 1) return { group: 2, color: '#ff6666' }; // send is "a bit greater" than receive (lighter red)
-    if (ratio === 1) return { group: 3, color: 'cyan' }; // send is "equal" to receive
-    if (ratio > 0.8) return { group: 4, color: '#66ff66' }; // receive is "a bit greater" than send (lighter green)
-    return { group: 5, color: 'green' }; // receive is "much greater" than send
+    if (ratio > 1.2) return { group: 1, color: '#FF0000' }; // send is "much greater" than receive
+    if (ratio > 1) return { group: 2, color: '#A00000' }; // send is "a bit greater" than receive (lighter red)
+    if (ratio === 1) return { group: 3, color: '#0dd' }; // send is "equal" to receive
+    if (ratio > 0.8) return { group: 4, color: '#00B000' }; // receive is "a bit greater" than send (lighter green)
+    return { group: 5, color: '#00FF00' }; // receive is "much greater" than send
 }
 
 
@@ -41,8 +61,8 @@ function determineGroup(address, TxSummaryData) {
 function computeNodeRadius(address, TxSummaryData) {
     const maxReceived = TxSummaryData.result[0].received;
     const minReceived = TxSummaryData.result[TxSummaryData.result.length - 1].received;
-    const maxRadius = 30;
-    const minRadius = 2;
+    const maxRadius = 10;
+    const minRadius = 3;
 
     const entry = TxSummaryData.result.find(item => item.address === address);
     if (!entry) return minRadius;
@@ -53,11 +73,29 @@ function computeNodeRadius(address, TxSummaryData) {
 
 function DashboardContent() {
     const fgRef = useRef();
-    const [fullScreenToggle, setfullScreenToggle] = useState(false);
+    const [fullScreenToggle, setfullScreenToggle] = useState(true);
     const { defaultData, setDefaultData } = useContext(GeneralContext);
     const { filteredtxData } = useContext(GeneralContext);
     const { txVisualData } = useContext(GeneralContext);
-    const { TxSummaryData, setTxSummaryData } = useContext(GeneralContext);
+    const { TxSummaryData} = useContext(GeneralContext);
+    const web3 = new Web3(Web3.givenProvider);
+
+    // Assuming you have state for the time range:
+    const [timeRange, setTimeRange] = useState({
+        start: new Date(),
+        end: new Date()
+    });
+
+
+    const handleStartDateChange = (date) => {
+        setTimeRange(prev => ({ ...prev, start: date.toDate() }));
+    };
+    
+    const handleEndDateChange = (date) => {
+        setTimeRange(prev => ({ ...prev, end: date.toDate() }));
+    };
+    
+  
 
     const handleNodeClick = useCallback(node => {
         const { nodes, links } = defaultData;
@@ -92,6 +130,28 @@ function DashboardContent() {
         console.log('defaultData:', defaultData);
     }, [defaultData]);
 
+
+    
+
+    function getMaxMinLinkValue(links) {
+        let max = -Infinity;
+        let min = Infinity;
+    
+        links.forEach(link => {
+            if (link.value > max) max = link.value;
+            if (link.value < min) min = link.value;
+        });
+    
+        return { max, min };
+    }
+    
+    function computeLinkWidth(value, max, min) {
+        const MIN_WIDTH = 1;  // minimum link width
+        const MAX_WIDTH = 5;  // maximum link width
+        return MIN_WIDTH + (value - min) * (MAX_WIDTH - MIN_WIDTH) / (max - min);
+    }
+
+    
     
     function getUniqueAddysForVisualizer(data) {
         let temp = { nodes: [], links: [] };
@@ -102,6 +162,7 @@ function DashboardContent() {
             let to_address_friendlyName = data[i].to_address_friendlyName;
             let from_address_exists = false;
             let to_address_exists = false;
+            let txValue = web3.utils.fromWei(data[i].value, 'ether');
 
             for (let j = 0; j < temp.nodes.length; j++) {
                 if (temp.nodes[j].id === from_address_friendlyName) {
@@ -136,7 +197,7 @@ function DashboardContent() {
                 });
             }
             if (displayAddressFN(from_address_friendlyName) && displayAddressFN(to_address_friendlyName)) {
-                temp.links.push({ source: displayAddressFN(from_address_friendlyName), target: displayAddressFN(to_address_friendlyName) });
+                temp.links.push({ source: displayAddressFN(from_address_friendlyName), target: displayAddressFN(to_address_friendlyName), value: txValue});
             }
             temp.nodes = temp.nodes.filter((thing, index, self) =>
                 index === self.findIndex((t) => (
@@ -144,24 +205,140 @@ function DashboardContent() {
                 ))
             )
         }
+        const { max, min } = getMaxMinLinkValue(temp.links);       
+        // Adjust link width based on its value
+        temp.links = temp.links.map(link => ({
+            ...link,
+            width: computeLinkWidth(link.value, max, min)
+        }));
+
+        
+
+        
         return temp;
     }
 
-    useEffect(() => {
+    useEffect(() => { 
+        let dataToSet;
+    
         if (filteredtxData && txVisualData && filteredtxData.length > 0 && txVisualData.length > 0) {
-            setDefaultData(getUniqueAddysForVisualizer(filteredtxData));
+            dataToSet = getUniqueAddysForVisualizer(filteredtxData);
+        } else if (txVisualData && txVisualData.length > 0) {
+            console.log('txVisualData: ', txVisualData);
+            dataToSet = getUniqueAddysForVisualizer(txVisualData);
         }
-        else if (txVisualData && txVisualData.length > 0) {
-            setDefaultData(getUniqueAddysForVisualizer(txVisualData));
+    
+        if (dataToSet) {
+            const { max, min } = getMaxMinLinkValue(dataToSet.links);
+    
+            // Adjust link width and distance based on its value
+            dataToSet.links = dataToSet.links.map(link => ({
+                ...link,
+                width: computeLinkWidth(parseFloat(link.value), max, min),
+            }));
+    
+            setDefaultData(dataToSet);
         }
     }, [filteredtxData, txVisualData]);
+    
+    const [subRange, setSubRange] = useState({
+        start: new Date(),  // You can set default values here
+        end: new Date()     // You can set default values here
+    });
+
+    const [sliderValue, setSliderValue] = useState(50);  // Initial value set to 50
+
+    const handleSliderChange = (value) => {
+        setSliderValue(value);
+    };
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'left', alignItems: 'center', position: 'absolute', bottom: '1vh', width: '100vw', height: '91vh', }}>
-            <div style={{ display: 'flex', borderRadius: '0.5vh', justifyContent: 'center', alignItems: 'center', position: 'absolute', left: '0.5vw', top: '1%', width: '75vw', height: '18%', border: '1px solid rgba(255,255,255,0.1)' }}>
-                Filter Panel
+        <div style={{ display: 'flex', justifyContent: 'left', alignItems: 'center', position: 'absolute', bottom: '0vh', width: '100vw', height: '91vh', }}>
+            <div style={{ zIndex: '50', display: 'flex', backgroundColor: 'rgba(20,20,20,0.6)', borderRadius: '0.5vh', alignItems: 'center', position: 'absolute', left: '1vw', top: '1%', width: '80vw', height: '9vh', border: '1px solid rgba(255,255,255,0.1)', color: '#fff' }}>
+                <TextField 
+                    label="Search" 
+                    variant="outlined" 
+                    size="small" 
+                    style={{border:'1px solid rgba(255,255,255,0.2)',borderRadius:'0.3vw', width: '25%', marginLeft:'0.5vw', color: '#fff' }}
+                    InputLabelProps={{
+                        style: { color: '#fff' },
+                    }}
+                    inputProps={{
+                        style: { color: '#fff' },
+                    }}
+                />
+                <div style={{ height:'100%', width:'100%',position:'absolute', display:'flex', alignItems:'end', paddingBottom:'0.5vh', left:'30vw', marginLeft: '1vw', marginRight: '1vw' }}>
+                    {/* {timeRange.start.toLocaleDateString()} - {timeRange.end.toLocaleDateString()} */}
+                    <div style={{
+                        border: '0px solid #0f0',
+                        position: 'absolute',
+                        width: '35.4vw',
+                        height: '90%',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'start',  
+                        // background: 'linear-gradient(90deg, rgba(20,20,20,1) 0%, rgba(60,60,60,1) 100%)',  
+                        padding: '10px 0'  
+                    }}>
+                        <ReactSlider
+                            min={1}
+                            max={100}
+                            value={sliderValue}
+                            onChange={handleSliderChange}
+                            className="my-slider"
+                            thumbClassName="my-slider-thumb"
+                        />
+                    </div>
+
+
+
+                    <Datetime 
+                        className="customDatetime"
+                        value={timeRange.start}
+                        onChange={date => setTimeRange({ ...timeRange, start: date })}
+                        inputProps={{
+                            placeholder: "Start Date",
+                            style: { color: '#fff', fontSize: '1.2vw', width:'13vw', backgroundColor: 'rgba(20,20,20,0.6)', border: '1px solid #fff',   }
+                        }}
+                    />
+                    <div style={{height:'100%', display:'flex', alignItems:'end',}}>
+                        <IconButton color="primary" style={{ marginLeft: '10px', marginRight: '10px' }}>
+                            <FastRewindIcon />
+                        </IconButton>
+                        <IconButton color="primary" style={{ marginLeft: '10px', marginRight: '10px' }}>
+                            <PlayArrowIcon />
+                        </IconButton>
+                        <IconButton color="primary" style={{ marginLeft: '10px', marginRight: '10px' }}>
+                            <FastForwardIcon />
+                        </IconButton>
+                    </div>
+
+                    <Datetime 
+                        className="customDatetime"
+                        value={timeRange.end}
+                        onChange={date => setTimeRange({ ...timeRange, end: date })}
+                        inputProps={{
+                            placeholder: "End Date",
+                            style: { color: '#fff', fontSize: '1.2vw', width:'13vw', backgroundColor: 'rgba(20,20,20,0.6)', border: '1px solid #fff' },
+                            
+                            
+                        }}
+                    />
+                    
+                </div>
+                
+                <Button 
+                    variant="contained" 
+                    color="secondary" 
+                    startIcon={<ClearIcon />} 
+                    style={{ position:'absolute', right:'1%', marginLeft: '10px', marginRight: '10px' }}
+                >
+                    Clear
+                </Button>
             </div>
-            <div style={{ display: 'flex', borderRadius: '0.5vh', justifyContent: 'center', alignItems: 'center', position: 'absolute', right: '0.5vw', top: '1%', width: '23.5vw', height: '99%', border: '1px solid rgba(255,255,255,0.1)' }}>
+
+
+            <div style={{ zIndex:'50', display: 'flex', borderRadius: '0.5vh', justifyContent: 'center', alignItems: 'center', position: 'absolute', right: '1vw', top: '1%', width: '17vw', height: '40vh', border: '1px solid rgba(255,255,255,0.1)' }}>
                 TX Details
             </div>
             <div className={fullScreenToggle ? "expandedTxView" : "fitTxView"}>
@@ -178,25 +355,60 @@ function DashboardContent() {
                 ref={fgRef}
                 graphData={defaultData}
                 // nodeAutoColorBy="group"
+                
+                linkDistance={() => 5}  // fixed distance 
                 nodeColor={node => node.color}
                 linkHoverPrecision={10}
                 backgroundColor="rgba(5,5,8,1)"
                 nodeLabel={function (d) {
                     return "<div style='color: #111; background-color: #999; border: 1px solid rgba(255,255,255,0.5); border-radius:0.5vh; padding:0.5vh'; text-align:center><span class='label'>Name: " + d.id + "<br />...</span></div>";
                 }}
+
                 nodeThreeObject={(node) => {
-                    const sphereGeometry = new THREE.SphereGeometry(node.radius);
-                    const material = new THREE.MeshBasicMaterial({ color: node.color });
-                    return new THREE.Mesh(sphereGeometry, material);
-                }}
+                    const group = new THREE.Group();
+                
+                    // Main sphere (fill color)
+                    const mainSphereGeometry = new THREE.SphereGeometry(node.radius);
+                    const mainMaterial = new THREE.MeshBasicMaterial({ color: node.color });
+                    const mainSphere = new THREE.Mesh(mainSphereGeometry, mainMaterial);
+                    group.add(mainSphere);
+                
+                    // Create a circular texture
+                    const canvas = document.createElement('canvas');
+                    canvas.width = 64;
+                    canvas.height = 64;
+                    const context = canvas.getContext('2d');
+                    context.arc(32, 32, 30, 0, 2 * Math.PI); // Adjust the radius as needed
+                    context.fillStyle = 'rgba(0,0,0,1)';
+                    context.fill();
+                    const texture = new THREE.CanvasTexture(canvas);
+                
+                    // Sprite for the border
+                    const spriteMaterial = new THREE.SpriteMaterial({
+                        map: texture,
+                        transparent: true,
+                        depthTest: false,  // Ensure sprite is always rendered on top
+                        depthWrite: false  // Prevent sprite from affecting depth buffer
+                    });
+                    const sprite = new THREE.Sprite(spriteMaterial);
+                    sprite.scale.set(node.radius * 0.03, node.radius * 0.03, 1); // Adjust the multiplier as needed to control the border size
+                    group.add(sprite);
+                
+                    return group;
+                  }}
+
+                // linkWidth={link => link.width}
+                
+
                 enableNodeDrag={false}
                 enableNavigationControls={true}
                 showNavInfo={true}
                 onNodeClick={handleNodeClick}
                 linkDirectionalParticles={1}
-                linkDirectionalParticleSpeed={0.005}
-                linkDirectionalParticleColor={() => "#0f0"}
+                linkDirectionalParticleSpeed={0.001}
+                linkDirectionalParticleColor={() => "#444"}
                 linkDirectionalParticleWidth={2}
+
             />
             </div>
         </div>
